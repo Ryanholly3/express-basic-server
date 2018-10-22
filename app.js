6240bc011f84e9d0d06aa783301d09cacc37a45d
ryanholly3 <ryanholly3@gmail.com>
2018-10-22 13:05:53 -0600

    added error handling

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+
 
 //DATA
 const characters = require('./data.json');
-console.log(characters)
+
+
+// General Middleware (applied to all requests)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res, next) =>{
   res.send('🐶')
@@ -16,20 +21,58 @@ app.get('/characters', (req, res, next) =>{
 })
 
 //loop
+// app.get('/characters/:id', (req, res, next) =>{
+//   for(let i = 0; i < characters.length; i++){
+//     let id = req.params.id;
+//     if(characters[i].id == id){
+//       res.json(characters[i])
+//     }
+//   }
+// })
+
+//filter
 app.get('/characters/:id', (req, res, next) =>{
-  for(let i = 0; i < characters.length; i++){
-    let id = req.params.id;
-    if(characters[i].id == id){
-      res.json(characters[i])
-    }
+  const id = req.params.id;
+  let convertInt =parseInt(id)
+  console.log(typeof convertInt)
+
+  if(typeof convertInt !== 'number'){
+    next({ error: 'not found', status: 404})
+  } else {
+    const character = characters.filter((character) => {
+      return character.id == id
+    })[0];
+    res.json({character: character});
   }
+
 })
 
+
+app.post('/characters', (req, res, next) =>{
+  //pull the data that is to be posted from request body
+  const body = req.body;
+  characters.push(body)
+
+  res.json({ characters: characters })
+})
 //alternative solution \/
 // app.get('/characters/:id', (req, res, next) =>{
 //   res.json(characters[req.params.id - 1])
 // })
 // ^ this method is flawed if index does not start at 1!!!!
+
+app.use(notFound);
+// general purpose catch all
+app.use(errorHandler);
+
+function errorHandler(err, req, res, next) {
+  console.error('ERROR', err)
+  res.status(404).send({error: "not an integer"})
+}
+
+function notFound(req, res, next) {
+  res.status(404).send(err)
+}
 
 app.listen(port, ()=>{
   console.log(`Server running on port ${port}`)
